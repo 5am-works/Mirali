fcffc21e96a0ba8b97206dc3893c00030c51d673
Sam Wang <samofcorinth@gmx.ca>
2022-10-29 21:13:54 -0700

    Added other token types

--- src/tokenizer.nim
+++ src/tokenizer.nim
@@ -1,10 +1,17 @@
 import std/options
 import std/strformat
+import std/re
+
+let namePattern = re"^[A-Z][A-Za-z]*$"
+let integerPattern = re"^-?[1-9][0-9]*$"
+let decimalPattern = re"^-?[1-9][0-9]*\.[0-9]+$"
 
 type
    TokenType* = enum
       Integer
-      Plus
+      Identifer
+      Name
+      Decimal
    Token* = tuple[literal: string, tokenType: TokenType]
    Tokenizer = object
       input: string
@@ -18,14 +25,25 @@ proc readChar(t: var Tokenizer): char =
       t.position += 1
 
 proc readToken(t: var Tokenizer): Option[Token] =
-   let nextChar = t.readChar()
-   result = case nextChar:
-      of '+':
-         some((literal: $nextChar, tokenType: Plus))
-      of '\0':
-         none(Token)
+   let tokenStart = t.position
+   var nextChar = t.readChar()
+   var tokenEnd = tokenStart
+   while nextChar != '\0' and nextChar != ' ':
+      nextChar = t.readChar()
+      tokenEnd += 1
+   return if tokenStart == tokenEnd:
+      none(Token)
+   else:
+      let literal = t.input[tokenStart .. tokenEnd - 1]
+      let tokenType = if literal.match(namePattern):
+         Name
+      elif literal.match(integerPattern):
+         Integer
+      elif literal.match(decimalPattern):
+         Decimal
       else:
-         raise newException(Exception, &"Invalid character: {nextChar}")
+         Identifer
+      some((literal: literal, tokenType: tokenType))
 
 iterator tokenize*(input: string): Token =
    var tokenizer = Tokenizer(input: input, position: 0)
--- src/tokenizer_tests.nim
+++ src/tokenizer_tests.nim
@@ -3,9 +3,39 @@ import unittest
 import tokenizer
 
 suite "Tokenizer tests":
-   test "Single-character tokens":
-      let input = "+"
+   test "Operator identifiers":
+      let input = "+ - * /"
       let result = tokenize(input).toSeq
       check(result == @[
-         (literal: "+", tokenType: Plus),
+         (literal: "+", tokenType: Identifer),
+         (literal: "-", tokenType: Identifer),
+         (literal: "*", tokenType: Identifer),
+         (literal: "/", tokenType: Identifer),
+      ])
+
+   test "Number literals":
+      let input = "1 2 2.3"
+      let result = tokenize(input).toSeq
+      check(result == @[
+         (literal: "1", tokenType: Integer),
+         (literal: "2", tokenType: Integer),
+         (literal: "2.3", tokenType: Decimal),
+      ])
+   
+   test "Names":
+      let input = "X Alice Bob"
+      let result = tokenize(input).toSeq
+      check(result == @[
+         (literal: "X", tokenType: Name),
+         (literal: "Alice", tokenType: Name),
+         (literal: "Bob", tokenType: Name),
+      ])
+   
+   test "Function names":
+      let input = "print! println! define!"
+      let result = tokenize(input).toSeq
+      check(result == @[
+         (literal: "print!", tokenType: Identifer),
+         (literal: "println!", tokenType: Identifer),
+         (literal: "define!", tokenType: Identifer),
       ])
